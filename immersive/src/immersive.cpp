ac57a00f68612eebf5c781cc885dd5eb73e975ae
Panu <Broman.Panu.P@student.uta.fi>
2018-02-27 19:32:04 +0200

    Moved model loading to initialization, commented zbar out. Changed rendermode to continuous, now cppDraw() is called frequently

--- immersive/src/immersive.cpp
+++ immersive/src/immersive.cpp
@@ -8,8 +8,14 @@
 
 #include <osgDB/ReadFile>
 #include <osgDB/FileUtils>
-#include <osgViewer/Viewer>
+
 #include <osg/Version>
+#include <osgViewer/Viewer>
+#include <osgViewer/Renderer>
+#include <osgViewer/ViewerEventHandlers>
+#include <osg/GL>
+#include <osg/GLExtensions>
+
 #include <zbar.h>
 
 #include <string>
@@ -42,6 +48,7 @@ const static int BUFFER_SIZE = 128;
 #include "osgAndroidNotifier.h" //Handles logging osg notifications
 static immersive::OsgAndroidNotifier* _osgAndroidNotifier;
 
+static osgViewer::Viewer _viewer;
 //JNIEXPORT jint JNICALL
 //Java_net_immersive_immersiveclient_Hello_addInts(JNIEnv *env, jobject obj, jint a, jint b) {
 //	return a + b;
@@ -59,6 +66,10 @@ static immersive::OsgAndroidNotifier* _osgAndroidNotifier;
 JNIEXPORT void JNICALL
 Java_net_immersive_immersiveclient_Immersive_cppInit(JNIEnv *env, jclass clss, jobject assetManager, jstring filePath){
 	if(!initialized){
+	LOGD("Initializing native");
+
+	const char *osg_ver = osgGetVersion();
+	LOGD("OSG version: %s\n", osg_ver);
 
 	_osgAndroidNotifier = new immersive::OsgAndroidNotifier();
     _osgAndroidNotifier->setTag("OSG NOTIFIER");
@@ -97,6 +108,30 @@ Java_net_immersive_immersiveclient_Immersive_cppInit(JNIEnv *env, jclass clss, j
 
 	AAssetDir_close(assetDir);
 
+	//OSG CODE ----------------------------------
+	
+	const char * testFileName = "/data/user/0/net.immersive.immersiveclient/files/cat.obj";
+
+	//osgDB::setCurrentWorkingDirectory("/data/user/0/net.immersive.immersiveclient/files/");
+
+    osg::ref_ptr<osg::Node> model = osgDB::readRefNodeFile( testFileName );
+	if(NULL != model){
+		LOGD("OSG SUCCESFULLY LOADED MODEL %s",testFileName);
+	} else {
+		LOGD("OSG FAILED TO LOAD MODEL %s",testFileName);
+	}
+    _viewer.setSceneData( model.get() );
+
+    _viewer.setUpViewerAsEmbeddedInWindow(0, 0, 1000, 1000);
+    _viewer.setThreadingModel(osgViewer::ViewerBase::SingleThreaded);
+
+	_viewer.realize();
+
+    _viewer.addEventHandler(new osgViewer::StatsHandler);
+    _viewer.addEventHandler(new osgViewer::ThreadingHandler);
+    _viewer.addEventHandler(new osgViewer::LODScaleHandler);
+	//_viewer.run();
+	
 	LOGD("Native initialized");
 	initialized = true;
 	}//end initialization block
@@ -109,113 +144,87 @@ Java_net_immersive_immersiveclient_Immersive_cppInit(JNIEnv *env, jclass clss, j
 // public static native void cppDraw(int bufferWidth, int bufferHeight, int format, int bytesPerPixel, ByteBuffer buffer);
 JNIEXPORT void JNICALL
 Java_net_immersive_immersiveclient_Immersive_cppDraw(JNIEnv *env, jclass cls, jint buffer_width, jint buffer_height, jint buffer_format, jint bytes_per_pixel, jobject jbuffer) {
-	const char *osg_ver = osgGetVersion();
-	LOGD("CppDraw was called, OSG: %s\n", osg_ver);
+
+	_viewer.frame();
+
+	//LOGD("cppDraw call");
 	
-	const char * testFileName = "/data/user/0/net.immersive.immersiveclient/files/cat.obj";
+	///* jobject buffer is of Java type java/nio/ByteBuffer*/
+	//unsigned char *buffer = (unsigned char *) env->GetDirectBufferAddress(jbuffer);
 
-	osgDB::setCurrentWorkingDirectory("/data/user/0/net.immersive.immersiveclient/files/");
-	LOGD("Current working dir %s",osgDB::getCurrentWorkingDirectory);
-	std::ifstream ifs(testFileName);
-	std::string str;
-	while(ifs >> str){
-		//LOGD("File contains: %s",str.c_str());
-	}
+	//unsigned char *grey = (unsigned char *) malloc(sizeof(unsigned char) * buffer_width * buffer_height);
+	//if(!grey) {
+	//	LOGE("failed to malloc()\n");
+	//	return;
+	//}
+	//
+	//int x, y;
+	//for(y = 0; y < buffer_width; y++) {
+	//	for(x = 0; x < buffer_height; x++) {
+	//		grey[y * buffer_width + x] = to_grayscale(buffer + y * buffer_width * bytes_per_pixel + x * bytes_per_pixel);
+	//	}
+	//}
+    //
+	//unsigned int major, minor;
+	//int ret = zbar_version(&major, &minor);
+	//LOGD("zbar major: %u, minor: %u, ret: %d\n", major, minor, ret);
 
-    osgViewer::Viewer viewer;
-    osg::ref_ptr<osg::Node> model = osgDB::readRefNodeFile( testFileName );
-	if(NULL != model){
-		LOGD("OSG SUCCESFULLY LOADED MODEL %s",testFileName);
-	} else {
-		LOGD("OSG FAILED TO LOAD MODEL %s",testFileName);
-	}
-    viewer.setSceneData( model.get() );
+	//zbar_image_scanner_t *zbar = zbar_image_scanner_create();
+	//if(!zbar) {
+	//	LOGE("failed to construct zbar\n");
+	//	free(grey);
+	//	return;
+	//}
 
-    viewer.setUpViewerAsEmbeddedInWindow(300, 300, 300, 300);
-    viewer.setThreadingModel(osgViewer::ViewerBase::SingleThreaded);
+	//zbar_image_t *zimg = zbar_image_create();
+	//if(!zimg) {
+	//	LOGE("failed to construct zbar image\n");
+	//	free(grey);
+	//	return;
+	//}
 
-	viewer.realize();
+	//zbar_image_set_format(zimg, 0x59455247);
+	//zbar_image_set_size(zimg, buffer_width, buffer_height);
+	//zbar_image_set_data(zimg, grey, buffer_width * buffer_height, NULL);
 
-	//viewer.run();
+	//ret = zbar_scan_image(zbar, zimg);
 
-	viewer.frame();
-	
-	/* jobject buffer is of Java type java/nio/ByteBuffer*/
-	unsigned char *buffer = (unsigned char *) env->GetDirectBufferAddress(jbuffer);
+	//const zbar_symbol_set_t *symbols = zbar_image_scanner_get_results(zbar);
+	//if(!symbols) {
+	//	LOGE("failed to get symbols\n");
+	//	free(grey);
+	//	return;
+	//}
 
-	unsigned char *grey = (unsigned char *) malloc(sizeof(unsigned char) * buffer_width * buffer_height);
-	if(!grey) {
-		LOGE("failed to malloc()\n");
-		return;
-	}
-	
-	int x, y;
-	for(y = 0; y < buffer_width; y++) {
-		for(x = 0; x < buffer_height; x++) {
-			grey[y * buffer_width + x] = to_grayscale(buffer + y * buffer_width * bytes_per_pixel + x * bytes_per_pixel);
-		}
-	}
-    
-	unsigned int major, minor;
-	int ret = zbar_version(&major, &minor);
-	LOGD("zbar major: %u, minor: %u, ret: %d\n", major, minor, ret);
+	//const zbar_symbol_t *sym = zbar_symbol_set_first_symbol(symbols);
+	//if(!sym) {
+	//	LOGE("No symbols found\n");
+	//	free(grey);
+	//	return;
+	//}
 
-	zbar_image_scanner_t *zbar = zbar_image_scanner_create();
-	if(!zbar) {
-		LOGE("failed to construct zbar\n");
-		free(grey);
-		return;
-	}
+	//while(sym) {
+	//	const char *data = zbar_symbol_get_data(sym);
+	//	if(!data) {
+	//		LOGW("failed to get data from symbol");
+	//	} else {
+	//		unsigned int npoints = zbar_symbol_get_loc_size(sym);
+	//		unsigned int i;
 
-	zbar_image_t *zimg = zbar_image_create();
-	if(!zimg) {
-		LOGE("failed to construct zbar image\n");
-		free(grey);
-		return;
-	}
+	//		for(i = 0; i < npoints; i++) {
+	//			LOGD("  * point %d (x, y): (%3d, %3d)\n", i,
+	//					zbar_symbol_get_loc_x(sym, i),
+	//					zbar_symbol_get_loc_y(sym, i));
+	//		}
+	//		
+	//		LOGD("data within symbol: %s\n", data);
+	//	}
+	//	
+	//	sym = zbar_symbol_next(sym);
+	//}
 
-	zbar_image_set_format(zimg, 0x59455247);
-	zbar_image_set_size(zimg, buffer_width, buffer_height);
-	zbar_image_set_data(zimg, grey, buffer_width * buffer_height, NULL);
-
-	ret = zbar_scan_image(zbar, zimg);
-
-	const zbar_symbol_set_t *symbols = zbar_image_scanner_get_results(zbar);
-	if(!symbols) {
-		LOGE("failed to get symbols\n");
-		free(grey);
-		return;
-	}
-
-	const zbar_symbol_t *sym = zbar_symbol_set_first_symbol(symbols);
-	if(!sym) {
-		LOGE("No symbols found\n");
-		free(grey);
-		return;
-	}
-
-	while(sym) {
-		const char *data = zbar_symbol_get_data(sym);
-		if(!data) {
-			LOGW("failed to get data from symbol");
-		} else {
-			unsigned int npoints = zbar_symbol_get_loc_size(sym);
-			unsigned int i;
-
-			for(i = 0; i < npoints; i++) {
-				LOGD("  * point %d (x, y): (%3d, %3d)\n", i,
-						zbar_symbol_get_loc_x(sym, i),
-						zbar_symbol_get_loc_y(sym, i));
-			}
-			
-			LOGD("data within symbol: %s\n", data);
-		}
-		
-		sym = zbar_symbol_next(sym);
-	}
-
-	zbar_image_destroy(zimg);
-	zbar_image_scanner_destroy(zbar);
+	//zbar_image_destroy(zimg);
+	//zbar_image_scanner_destroy(zbar);
 }
 #endif
 
